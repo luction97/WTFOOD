52bf33dea2ab9b08be2b7efd4171fcac9a4dc5ad
Tebanqito <yemesesteban@gmail.com>
2022-05-31 20:50:55 -0500

    Entidad Receta Completa

--- src/main/java/com/wtfood/entidades/Receta.java
+++ src/main/java/com/wtfood/entidades/Receta.java
@@ -1,7 +1,26 @@
-
 package com.wtfood.entidades;
 
+import java.util.List;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import javax.persistence.OneToOne;
+import lombok.Data;
+import org.hibernate.annotations.GenericGenerator;
 
+@Data
+@Entity
 public class Receta {
-    
+
+    @Id
+    @GeneratedValue(generator = "uuid")
+    @GenericGenerator(name = "uuid", strategy = "uuid2")
+    private String id;
+    @OneToMany
+    private List<Ingrediente> ingrediente;
+    private Integer cantidadIngredientes;
+    @OneToOne
+    private Foto foto;
+
 }
