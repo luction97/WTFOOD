9abaabadaef8fb1f706e97d56bcccf8d8ff49980
francogriffo <franco_griffo@hotmail.com>
2022-06-01 20:46:52 -0300

    Servicio Ingrediente en proceso

--- src/main/java/com/wtfood/servicios/IngredienteServicio.java
+++ src/main/java/com/wtfood/servicios/IngredienteServicio.java
@@ -1,6 +1,18 @@
 
 package com.wtfood.servicios;
 
+import com.wtfood.repositorios.IngredienteRepositorio;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
 public class IngredienteServicio {
+    
+    @Autowired  
+    private IngredienteRepositorio ingredienteRepositorio;
+    
+    @Autowired
+    private FotoServicio fotoServicio;
+    
 
 }
