b06c716a6d7134531393a7d176b116a23b710bb5
Tebanqito <yemesesteban@gmail.com>
2022-06-01 20:16:27 -0500

    Entidad Receta y su Repositorio

--- src/main/java/com/wtfood/entidades/Receta.java
+++ src/main/java/com/wtfood/entidades/Receta.java
@@ -1,8 +1,30 @@
 
 package com.wtfood.entidades;
 
+import java.util.List;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import javax.persistence.OneToOne;
+import lombok.Data;
+import org.hibernate.annotations.GenericGenerator;
 
+@Data
+@Entity
 public class Receta {
     
-
+    @Id
+    @GeneratedValue(generator = "uuid")
+    @GenericGenerator(name = "uuid", strategy = "uuid2")
+    private String id;
+    private Integer calificaciones;
+    private Integer cantidadIngredientes;
+    @OneToMany
+    private List<Ingrediente> ingredientes;
+    @OneToOne
+    private Usuario usuario;
+    @OneToOne
+    private Foto foto;
+    
 }
--- src/main/java/com/wtfood/repositorios/RecetaRepositorio.java
+++ src/main/java/com/wtfood/repositorios/RecetaRepositorio.java
@@ -1,6 +1,26 @@
 
 package com.wtfood.repositorios;
 
-public class RecetaRepositorio {
+import com.wtfood.entidades.Receta;
+import java.util.ArrayList;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
 
+@Repository
+public interface RecetaRepositorio extends JpaRepository<Receta, String> {
+
+    @Query("SELECT r FROM Receta r")
+    public ArrayList<Receta> listarRecetas();
+    
+    @Query("SELECT r FROM Receta r WHERE r.usuario.nombre LIKE :nombre")
+    public ArrayList<Receta> buscarRecetaPorNombreUsuario(@Param("nombre") String nombre);
+    
+    @Query("SELECT r FROM Receta r WHERE r.calificaciones = :calificacion")
+    public ArrayList<Receta> buscarRecetaPorCalificaciones(@Param("calificacion") Integer calificacion);
+    
+    @Query("SELECT r FROM Receta r WHERE r.cantidadIngredientes = :cantidadIngredientes")
+    public ArrayList<Receta> buscarRecetaPorCantidadIngredientes(@Param("cantidadIngredientes") Integer cantidadIngredientes);
+    
 }
