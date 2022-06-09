4df09a31072959cebdf14eb01f85afb725c7bd98
Vanesa <vanetorales98@gmail.com>
2022-06-09 16:35:31 -0300

    se crearon roles de usuario

---
+++ src/main/java/com/wtfood/configuracion/SeguridadConfiguracion.java
@@ -0,0 +1,59 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.wtfood.configuracion;
+
+import com.wtfood.servicios.UsuarioServicio;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+/**
+ *
+ * @author Vanesa
+ */
+@Configuration
+@EnableWebSecurity
+@EnableGlobalMethodSecurity(prePostEnabled = true)
+public class SeguridadConfiguracion extends WebSecurityConfigurerAdapter {
+    
+    @Autowired
+    public UsuarioServicio usuarioServicio;
+    
+    @Autowired
+    public void globalConfigure(AuthenticationManagerBuilder auth) throws Exception{
+        auth.userDetailsService(usuarioServicio)
+               .passwordEncoder(new BCryptPasswordEncoder());
+    }
+    
+    protected void configure (HttpSecurity http) throws Exception{
+        http.authorizeRequests()
+              .antMatchers("/admin/'")
+              .hasRole("ADMINISTRADOR")
+              .antMatchers("/css/'", "/js/'", "/img/'" , "/'").permitAll()
+              .and()
+              .formLogin()
+                .loginPage("/loginRegistro")
+                .loginProcessingUrl("/logincheck")
+                .usernameParameter("email")
+                .passwordParameter("clave")
+                .defaultSuccessUrl("/")
+                .permitAll()
+                .and()
+                .logout()
+                .logoutUrl("/logout")
+                .logoutSuccessUrl("/login?logout")
+                .permitAll()
+                .and()
+                .csrf().disable();
+    }
+            
+    
+}
---
+++ src/main/java/com/wtfood/controladores/UsuarioControlador.java
@@ -0,0 +1,41 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.wtfood.controladores;
+
+import com.wtfood.enumeraciones.Rol;
+import com.wtfood.servicios.UsuarioServicio;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+/**
+ *
+ * @author Vanesa
+ */
+@Controller
+@RequestMapping("/usuario")
+public class UsuarioControlador {
+    
+    @Autowired
+    private UsuarioServicio usuarioServicio;
+    
+    @PostMapping("/registro")
+    private String guardar(ModelMap modelo, @RequestParam String nombre, @RequestParam String apellido, @RequestParam String email, @RequestParam String nickname, @RequestParam String clave, @RequestParam Rol rol){
+        try{
+            usuarioServicio.guardar(nombre, apellido, email, nickname, clave, true, Rol.USUARIO);
+            modelo.put("Exito", "Usuario guardado con éxito");
+        }catch(Exception e){
+            modelo.put("Error", "Error al registrarse");
+        }
+        return "loginRegistro";
+    }
+    
+    
+    
+}
