98f37931647a9a4d1e8cbee45e42cfba64e256da
Lucas Siano <lucassound@live.com>
2022-06-24 20:51:56 -0300

    update

--- src/main/java/com/wtfood/controladores/PortalControlador.java
+++ src/main/java/com/wtfood/controladores/PortalControlador.java
@@ -1,25 +1,24 @@
-
 package com.wtfood.controladores;
-
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class PortalControlador {
-    
-    
-     @GetMapping("/")
+
+    @GetMapping("/")
     public String index() {
         return "index";
     }
-    
-        @GetMapping("/loginRegistro")
+
+    @GetMapping("/loginRegistro")
     public String loginRegistro() {
         return "loginRegistro";
     }
-    
-     
-    
-}
 
+    @GetMapping("/modificarUsuario")
+    public String modificarUsuario() {
+        return "modificarUsuario";
+    }
+
+}
