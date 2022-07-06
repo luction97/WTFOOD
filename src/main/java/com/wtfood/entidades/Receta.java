5e233a5cbcdd2b3f4ce49f90da664def7337ec62
Tebanqito <yemesesteban@gmail.com>
2022-07-05 20:04:18 -0500

    Actualizacion pasoAPaso Receta

--- src/main/java/com/wtfood/entidades/Receta.java
+++ src/main/java/com/wtfood/entidades/Receta.java
@@ -6,6 +6,7 @@ import java.util.List;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
+import javax.persistence.ManyToMany;
 import javax.persistence.OneToMany;
 import javax.persistence.OneToOne;
 import lombok.Data;
@@ -28,7 +29,7 @@ public class Receta {
     
     private ArrayList<String> pasoAPaso;
     
-    @OneToMany
+    @ManyToMany
     private List<Ingrediente> ingredientes;
 
 }
